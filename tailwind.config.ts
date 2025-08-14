4237b5bbe761f8e1937f81bbb4c697e513d058ed
clintjeff2 <clintjeff2@gmail.com>
2025-08-14 12:32:42 +0100

    chore: update tailwind.config.ts configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,6 +63,9 @@ const config: Config = {
         "primary-green-light": "#23f075",
         "primary-green-dark": "#00d455",
       },
+      fontFamily: {
+        sans: ['var(--font-inter)', 'system-ui', 'sans-serif'],
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
